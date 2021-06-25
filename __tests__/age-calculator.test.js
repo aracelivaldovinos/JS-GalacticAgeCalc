5a99d92dc8bf7feb1d3a4d1b7c1340a83308c67f
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-25 11:06:25 -0500

    add a planet property toconstructor

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -3,13 +3,14 @@ import Calculator from './../src/age-calculator.js';
 describe('Calculator', () => {
 
   test('should correctly create a calculator object with three properties', () => {
-    const calculator = new Calculator (27,46, 0);
+    const calculator = new Calculator (27,46, 0, "jupiter");
     expect(calculator.age).toEqual(27);
     expect(calculator.yearsLeft).toEqual(46);
     expect(calculator.yearsOver).toEqual(0);
+    expect(calculator.planet).toEqual("jupiter");
   });
   test('should calculate age in mercury years', () => {
-    const mercuryAge = new Calculator (27,46, 0);
+    const mercuryAge = new Calculator (27,46, 0, "mercury");
     expect(mercuryAge.ageCalculator ()).toEqual(6);
   });
 });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,10 +1,13 @@
-export default function Calculator (age, yearsLeft, yearsOver) {
+export default function Calculator (age, yearsLeft, yearsOver, planet) {
   this.age = age;
   this.yearsLeft = yearsLeft;
   this.yearsOver = yearsOver
+  this.planet = planet
 }
 
 Calculator.prototype.ageCalculator = function () {
-  let mercury = Math.round(this.age * 0.24)
-  return mercury 
+  if (this.planet === "mercury") {
+    return (Math.round(this.age * 0.24))
+  }
+  
 }
