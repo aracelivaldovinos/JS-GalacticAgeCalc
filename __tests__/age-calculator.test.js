4bf1040b52426bb8205e37dc925db32ae554b9dc
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-25 14:00:23 -0500

    WOP: method for years over life expectancy

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -3,87 +3,86 @@ import Calculator from './../src/age-calculator.js';
 describe('Calculator', () => {
 
   test('should correctly create a calculator object with three properties', () => {
-    const calculator = new Calculator (27,46, 0, "jupiter", [0] );
+    const calculator = new Calculator (27,46, "jupiter");
     expect(calculator.age).toEqual(27);
     expect(calculator.lifeExpectancy).toEqual(46);
-    expect(calculator.newAge).toEqual(0);
     expect(calculator.planet).toEqual("jupiter");
  
   });
   test('should calculate age in mercury years', () => {
-    const mercuryAge = new Calculator (27,46, 0, "mercury");
-    expect(mercuryAge.ageCalculator ()).toEqual([6]);
+    const mercuryAge = new Calculator (27,46, "mercury");
+    expect(mercuryAge.ageCalculator ()).toEqual(6);
   });
   test('should calculate age in venus years', () => {
-    const venusAge = new Calculator (27,46, 0, "venus");
-    expect(venusAge.ageCalculator ()).toEqual([17]);
+    const venusAge = new Calculator (27,46, "venus");
+    expect(venusAge.ageCalculator ()).toEqual(17);
   });
   test('should calculate age in mars years', () => {
-    const marsAge = new Calculator (27,46, 0, "mars");
-    expect(marsAge.ageCalculator ()).toEqual([51]);
+    const marsAge = new Calculator (27,46, "mars");
+    expect(marsAge.ageCalculator ()).toEqual(51);
   });
   test('should calculate age in jupiter years', () => {
-    const jupiterAge = new Calculator (27,46, 0, "jupiter");
-    expect(jupiterAge.ageCalculator ()).toEqual([320]);
+    const jupiterAge = new Calculator (27,46, "jupiter");
+    expect(jupiterAge.ageCalculator ()).toEqual(320);
   });
   test('should calculate age in saturn years', () => {
-    const saturnAge = new Calculator (27,46, 0, "saturn");
-    expect(saturnAge.ageCalculator ()).toEqual([795]);
+    const saturnAge = new Calculator (27,46, "saturn");
+    expect(saturnAge.ageCalculator ()).toEqual(795);
   });
   test('should calculate age in uranus years', () => {
-    const uranusAge = new Calculator (27,46, 0, "uranus");
-    expect(uranusAge.ageCalculator ()).toEqual([2268]);
+    const uranusAge = new Calculator (27,46, "uranus");
+    expect(uranusAge.ageCalculator ()).toEqual(2268);
   });
   test('should calculate age in neptune years', () => {
-    const neptuneAge = new Calculator (27,46, 0, "neptune");
-    expect(neptuneAge.ageCalculator ()).toEqual([4449]);
+    const neptuneAge = new Calculator (27,46, "neptune");
+    expect(neptuneAge.ageCalculator ()).toEqual(4449);
   });
   test('should calculate age in pluto years', () => {
-    const plutoAge = new Calculator (27,46, 0, "pluto");
-    expect(plutoAge.ageCalculator ()).toEqual([6712]);
+    const plutoAge = new Calculator (27,46, "pluto");
+    expect(plutoAge.ageCalculator ()).toEqual(6712);
   });
   test('should calculate age in earth years', () => {
-    const earthAge = new Calculator (27,46, 0, "earth");
-    expect(earthAge.ageCalculator ()).toEqual([27]);
+    const earthAge = new Calculator (27,46, "earth");
+    expect(earthAge.ageCalculator ()).toEqual(27);
   });
   test('should calculate years left on mercury', () => {
-    const yearsLeft = new Calculator (27,73, [6], "mercury");
-    expect(yearsLeft.yearsLeft ()).toEqual(12);
+    const yearsLeft = new Calculator (27,73, "mercury");
+    expect(yearsLeft.yearsLeft ()).toEqual(11);
   });
   test('should calculate years left on venus', () => {
-    const yearsLeft = new Calculator (27,73, [17], "venus");
-    expect(yearsLeft.yearsLeft ()).toEqual(28);
+    const yearsLeft = new Calculator (27,73, "venus");
+    expect(yearsLeft.yearsLeft ()).toEqual(29);
   });
   test('should calculate years left on mars', () => {
-    const yearsLeft = new Calculator (27,73, [51], "mars",);
+    const yearsLeft = new Calculator (27,73, "mars",);
     expect(yearsLeft.yearsLeft ()).toEqual(86);
   });
   test('should calculate years left on jupiter', () => {
-    const yearsLeft = new Calculator (27,73, [320], "jupiter");
-    expect(yearsLeft.yearsLeft ()).toEqual(547);
+    const yearsLeft = new Calculator (27,73, "jupiter");
+    expect(yearsLeft.yearsLeft ()).toEqual(546);
   });
   test('should calculate years left on saturn', () => {
-    const yearsLeft = new Calculator (27,73, [795], "saturn");
-    expect(yearsLeft.yearsLeft ()).toEqual(1356);
+    const yearsLeft = new Calculator (27,73, "saturn");
+    expect(yearsLeft.yearsLeft ()).toEqual(1355);
   });
   test('should calculate years left on uranus', () => {
-    const yearsLeft = new Calculator (27,73, [2268], "uranus");
-    expect(yearsLeft.yearsLeft ()).toEqual(3865);
+    const yearsLeft = new Calculator (27,73, "uranus");
+    expect(yearsLeft.yearsLeft ()).toEqual(3864);
   });
   test('should calculate years left on neptune', () => {
-    const yearsLeft = new Calculator (27,73, [4449], "neptune");
-    expect(yearsLeft.yearsLeft ()).toEqual(7581);
+    const yearsLeft = new Calculator (27,73, "neptune");
+    expect(yearsLeft.yearsLeft ()).toEqual(7580);
   });
   test('should calculate years left on pluto', () => {
-    const yearsLeft = new Calculator (27,73, [6712], "pluto");
+    const yearsLeft = new Calculator (27,73, "pluto");
     expect(yearsLeft.yearsLeft ()).toEqual(11435);
   });
-  test('should calculate years left on pluto', () => {
-    const yearsLeft = new Calculator (27,73, [27], "earth");
+  test('should calculate years left on earth', () => {
+    const yearsLeft = new Calculator (27,73, "earth");
     expect(yearsLeft.yearsLeft ()).toEqual(46);
   });
   test('should calculate years over life expectancy on mercury', () => {
-    const yearsOver = new Calculator (27,73, [27], "earth");
+    const yearsOver = new Calculator (27,73, [27], "mercury");
     expect(yearsOver.yearsOver ()).toEqual(46);
   });
   
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,66 +1,58 @@
-export default function Calculator (age, lifeExpectancy, newAge, planet) {
+export default function Calculator (age, lifeExpectancy, planet) {
   this.age = age;
   this.lifeExpectancy = lifeExpectancy;
-  this.newAge = newAge
   this.planet = planet
 }
 
 Calculator.prototype.ageCalculator = function () {
-  this.newAge = []
   if (this.planet === "mercury") {
-    this.newAge.push((Math.round(this.age * 0.24)))
-    return this.newAge
+    return((Math.round(this.age * 0.24)))
   }else if (this.planet === "venus") {
-    this.newAge.push((Math.round(this.age * 0.62)))
-    return this.newAge
+    return((Math.round(this.age * 0.62)))
   }else if (this.planet === "mars") {
-    this.newAge.push((Math.round(this.age * 1.88)))
-    return this.newAge
+    return((Math.round(this.age * 1.88)))
   }else if (this.planet === "jupiter") {
-    this.newAge.push((Math.round(this.age * 11.86)))
-    return this.newAge
+    return((Math.round(this.age * 11.86)))
   }else if (this.planet === "saturn") {
-    this.newAge.push((Math.round(this.age * 29.46)))
-    return this.newAge
+    return((Math.round(this.age * 29.46)))
   }else if (this.planet === "uranus") {
-    this.newAge.push((Math.round(this.age * 84.01)))
-    return this.newAge
+    return((Math.round(this.age * 84.01)))
   }else if (this.planet === "neptune") {
-    this.newAge.push((Math.round(this.age * 164.79)))
-    return this.newAge
+    return((Math.round(this.age * 164.79)))
   }else if (this.planet === "pluto") {
-    this.newAge.push((Math.round(this.age * 248.59)))
-    return this.newAge
+    return((Math.round(this.age * 248.59)))  
 }else {
-  this.newAge.push((Math.round(this.age * 1)))
-  return this.newAge
+  return((Math.round(this.age * 1)))
 }
 }
 
 Calculator.prototype.yearsLeft = function () {
-  this.newAge = parseInt(this.newAge.toString())
+
   if (this.planet === "mercury") {
-    return Math.round(((this.lifeExpectancy * 0.24) - this.newAge))
+    return Math.round(((this.lifeExpectancy * 0.24) - (this.age * 0.24)))
   } else if (this.planet === "venus") {
-    return Math.round(((this.lifeExpectancy * 0.62) - this.newAge))
+    return Math.round(((this.lifeExpectancy * 0.62) - (this.age * 0.62)))
 }else if (this.planet === "mars") {
-  return Math.round(((this.lifeExpectancy * 1.88) - this.newAge))
+  return Math.round(((this.lifeExpectancy * 1.88) - (this.age * 1.88)))
 }else if (this.planet === "jupiter") {
-  return Math.round(((this.lifeExpectancy * 11.88) - this.newAge))
+  return Math.round(((this.lifeExpectancy * 11.86) - (this.age * 11.86)))
 }else if (this.planet === "saturn") {
-  return Math.round(((this.lifeExpectancy * 29.46) - this.newAge))
+  return Math.round(((this.lifeExpectancy * 29.46) - (this.age * 29.46)))
 }else if (this.planet === "uranus") {
-  return Math.round(((this.lifeExpectancy * 84.01) - this.newAge))
+  return Math.round(((this.lifeExpectancy * 84.01) - (this.age * 84.01)))
 }else if (this.planet === "neptune") {
-  return Math.round(((this.lifeExpectancy * 164.79) - this.newAge))
+  return Math.round(((this.lifeExpectancy * 164.79) - (this.age * 164.79)))
 }else if (this.planet === "pluto") {
-  return Math.round(((this.lifeExpectancy * 248.59) - this.newAge))
+  return Math.round(((this.lifeExpectancy * 248.59) - (this.age * 248.59)))
 }else {
-  return Math.round(((this.lifeExpectancy * 1) - this.newAge))
+  return Math.round(((this.lifeExpectancy * 1) - (this.age * 1)))
 }
 }
   
 Calculator.prototype.yearsOver = function () {
- 
+  if (this.planet === "mercury" && (this.lifeExpectancy * 0.24) <= (this.age*0.24)) {
+    return ((this.age*0.24) - (this.lifeExpectancy * 0.24) )
+
+}
 }
   
