204d50356f016d5769057027b73518b1bbc44178
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-12-17 12:33:19 -0600

    add UI

--- src/main.js
+++ src/main.js
@@ -1,23 +1,66 @@
-
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
-// // import Calculator from './age-calculator.js';
-
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Calculator from './age-calculator.js';
 
 
 
-// // $(document).ready(function(){
-// //   $("button#btn").on("click", function(event){
-// //     event.preventDefault();
-// //     let age = $("input#age").val();
-// //     this.name = $("input#name").val();
-// //     this.age= parseInt(age);
-// //     console.log(this.age);
-// //     console.log(this.name);
-// //   });
+
+$(document).ready(function(){
+  $("button").on("click", function(event){
+    event.preventDefault();
+    $("#planetsDiv").show();
+    // alert("it works")
+    let age = $("input#age").val();
+    this.age= parseInt(age);
+    this.name = $("input#name").val();
+    this.planet = $("select#planet").val();
+    let planet = this.planet[0].toUpperCase() + this.planet.substring(1);
+    // this.planet = planet.toUpperCase();
+
+    let person = new Calculator(this.age, 80, this.planet);
+    let years = person.ageCalculator();
+    let yearsLeft = person.yearsLeft();
+    let yearsOver = person.yearsOver();
+  
+
+    console.log(this.age);
+    console.log(this.name);
+    console.log(this.planet);
+
+    $(".userName").append(this.name);
+    $(".userAge").append(this.age);
+    $(".planetName").append(planet);
+    $(".years").append(years);
 
 
-// // });
+    if (this.age > 80){
+      $(".yearsOver").append(yearsOver);
+      $(".yearsOverLine").show();
+      $(".yearsLeftLine").hide();
+    }
+    else if (this.age === 80){
+      $(".yearsOverLine").empty();
+      $(".yearsOverLine").append("<p>You are the average life expectancy.</p>");
+      $(".yearsOverLine").show();
+    } 
+    else {
+      $(".yearsLeft").append(yearsLeft);
+      $(".yearsLeftLine").show();
+      $(".yearsOverLine").hide();
+    }
+
+    $(".form-group").click(function () {
+      $("#planetsDiv").hide();
+      $(".userName").empty();
+      $(".userAge").empty();
+      $(".planetName").empty();
+      $(".years").empty(years);
+      $(".yearsOver").empty(yearsOver);
+      $(".yearsLeft").empty(yearsLeft);
+    });
+   
+  });
+});
 
